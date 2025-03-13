176f76ac98c3ad9c9cab4f708677e25b1d83e4c8
WindowsVistaisCool <kyle@quackings.com>
2025-03-13 10:25:22 -0400

    dev

--- pi/drivetrain.py
+++ pi/drivetrain.py
@@ -11,11 +11,11 @@ class MecanumIOMap:
         self.br_l = br_l
         self.bl_h = bl_h
         self.bl_l = bl_l
-        self.all = [fl_h, fl_l, fr_h, fr_l]#, br_h, br_l, bl_h, bl_l]
+        self.all = [fl_h, fl_l, fr_h, fr_l, br_h, br_l, bl_h, bl_l]
         self.initOut()
     
     def initOut(self) -> None:
-        [gpio.setup(pin, gpio.OUT) if pin >= 0 else None for pin in self.all]
+        [gpio.setup(pin, gpio.OUT) for pin in self.all]
     
     def setSpeed(self, module: int, speed: int) -> None:
         high = self.all[module * 2]
@@ -36,16 +36,16 @@ class MecanumDrive:
     def drive(self, x, y, r) -> None:
         self.ioMap.setSpeed(0, y + x + r)
         self.ioMap.setSpeed(1, y - x - r)
-        # self.ioMap.setSpeed(2, y + x - r)
-        # self.ioMap.setSpeed(3, y - x + r)
+        self.ioMap.setSpeed(2, y + x - r)
+        self.ioMap.setSpeed(3, y - x + r)
     
     def stop(self) -> None:
         self.ioMap.stop()
 
 
-gpio.setmode(gpio.BOARD)
+gpio.setmode(gpio.BCM)
 
-ioMap = MecanumIOMap(5, 6, 13, 19, 0, 0, 0, 0)
+ioMap = MecanumIOMap(5, 6, 13, 19, 4, 17, 27, 2)
 drive = MecanumDrive(ioMap)
 
 while True:
