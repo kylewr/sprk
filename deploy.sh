17f1e492099a5f076d3e08cdbb4319a7fd4e6546
kylewr <kyle@quackings.com>
2026-01-20 23:44:35 -0500

    fix compilation on my p14s

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 cd ./build-rpi
 cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain-rpi.cmake ../src/ && cmake --build . --clean-first
-ssh pi@192.168.1.106 "rm /home/pi/robot"
-scp ./robot pi@192.168.1.106:/home/pi/
-ssh pi@192.168.1.106 "chmod +x /home/pi/robot"
+ssh pi@192.168.1.111 "rm /home/pi/robot"
+scp ./robot pi@192.168.1.111:/home/pi/
+ssh pi@192.168.1.111 "chmod +x /home/pi/robot"
