b99e955f2bc0c07673b2d6d771a4afcf0fbed414
kylewr <kyle@quackings.com>
2026-01-30 17:12:31 -0500

    delete robot.old

--- robot.old/autonomous/Simple.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-
-def run(robot):
-    robot.telemetry.log("Autonomous mode started.")
-    i = 0
-    while True:
-        i += 1
-        robot.telemetry.log(f"Autonomous loop iteration: {i}")
-        sleep(1)
--- robot.old/autonomous/__init__.py
+++
@@ -1,4 +0,0 @@
-from autonomous.CoolDrive import CoolDrive
-from autonomous.Demo import Demo
-from autonomous.DrivetrainTest import DrivetrainTest
-from autonomous.StepperTest import StepperTest
